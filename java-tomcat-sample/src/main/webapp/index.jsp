bf4d45dc49015254d06554e809f3b0de2b706453
Marquise,Anderson <manderson@workconsultants.com>
2020-03-29 15:46:29 -0400

    Update to Index.html2

--- java-tomcat-sample/src/main/webapp/index.jsp
+++ java-tomcat-sample/src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2>Damn, it took us forever to ger this bitch working!</h2>
+<p> Hello world, welcome to the Jenkins training Course </p>
 </body>
 </html>
--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -25,14 +25,16 @@ public class TestGreeter {
   }
 
   @Test
-  public void greetShouldIncludeGreetingPhrase() {
+  public void greetShouldIncludeGreetingPhrase() 
+  {
     String someone = "World";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
     @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingMessage() 
+  {
     String someone = "World, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
